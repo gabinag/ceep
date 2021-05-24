6a4a48dd85f3b89ef3ae125a43555c4d7f33061a
Gabriela Nagamuta <gnagamuta03@gmail.com>
2021-05-24 18:55:14 -0300

    Add const createTask to organize the event

--- taskList.js
+++ taskList.js
@@ -1,7 +1,11 @@
-const newTask = document.querySelector('[data-form-button]')
-
-newTask.addEventListener('click', ()=> {
+const createTask = (evento) => {
+    evento.preventDefault() 
     const input = document.querySelector('[data-form-input]')
     const valor = input.value
     console.log(valor)
-})
+    input.value = " " //limpa o valor do campo
+}
+
+const newTask = document.querySelector('[data-form-button]')
+
+newTask.addEventListener('click', createTask)
