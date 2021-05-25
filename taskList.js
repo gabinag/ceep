eeb1c6d8897bf864117d26da3cb28f4baeee8914
Gabriela Nagamuta <gnagamuta03@gmail.com>
2021-05-25 12:47:50 -0300

    Create list item element and add class in it

--- taskList.js
+++ taskList.js
@@ -1,12 +1,19 @@
 const createTask = (evento) => {
     evento.preventDefault() 
+
     const input = document.querySelector('[data-form-input]')
     const valor = input.value
-    
-    const task = document.querySelector('[data-task]')
+
+    const list = document.querySelector('[data-list]') 
+    const task = document.createElement('li')
+
+    task.classList.add('task')  //adiciona tarefa a partir da class no css (task)
+
     const content = `<p class="content">${valor}</p>`
 
-    task.innerHTML = content 
+    task.innerHTML = content
+
+    list.appendChild(task)  //coloca task (filho) dentro de list (pai), li dentro de ul
     input.value = " " //limpa o valor do campo
 }
 
